7f9ba8ce78c31c87167a790e2d27b5b735228b79
Vrajal <vrajal99@gmail.com>
2025-09-17 13:02:16 -0700

    ADDED: Navbar added with nav links, css and icons

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -7,9 +7,18 @@ export const AppContextProvider = ({ children }) => {
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
   const [isSeller, setIsSeller] = useState(false);
-  const value = { navigate, user, setUser, isSeller, setIsSeller };
-  return;
-  <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+  const [showUserLogin, setShowUserLogin] = useState(false);
+  const value = {
+    navigate,
+    user,
+    setUser,
+    isSeller,
+    setIsSeller,
+    showUserLogin,
+    setShowUserLogin,
+  };
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
 export const useAppContext = () => {
